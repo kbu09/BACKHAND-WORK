3bcd8e26e373cc91429400a3cb583ec1fe160391
kb <kbu8460@gmail.com>
2024-12-13 23:01:49 +0530

    Assignments

---
+++ Tops Work/PYTHON/Assessments/Assessments - 1/Billing_application.py
@@ -0,0 +1,18 @@
+
+
+# Assessment is pending due to some topics remaining 
+
+
+# Write a program to demonstrate the Billing application 
+#  Prepare demonstration of Python Billing application under software
+# development principles and follow coding protocols
+# 
+#  The project was built in a GUI application called tkinter module that
+# compose with different widgets to create an effective interactive interface.
+#  In this application all operations using MySQL queries 
+# 
+#  Create GUI application using tkinter module
+
+
+
+
